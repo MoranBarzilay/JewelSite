d8b15a68538e2d60407112e390f98d9ff85597cf
MoranBarzilay <moranbarzil4y@gmail.com>
2018-04-03 19:17:14 +0300

    First Upload

---
+++ JavaScript/LoadImages.js
@@ -0,0 +1,25 @@
+﻿function load(path) {
+    return function () {
+        $.ajax({
+            url: path,
+            success: function (data) {
+                var i = 0;
+                var imageCollection = $(data).find("a:contains(.jpeg)");
+                var length = imageCollection.length;
+                imageCollection.each(function () {
+                    var images = $(this).attr("href");
+                    $("#header ul").append('<li style="background-image: url(' + images + ');"></li>');
+                    if (i == length - 1) {
+                        $("#header ol").append('<li class="active" onclick="pointClick(' + i + ')"><i class="fa fa-circle-o"></i></li>');
+                    }
+                    else {
+                        $("#header ol").append('<li onclick="pointClick(' + i + ')"><i class="fa fa-circle-o"></i></li>');
+                    }
+                    i++;
+                });
+            }
+
+
+        });
+    }
+}
