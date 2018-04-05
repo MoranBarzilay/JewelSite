89b72c4b747be7a7d3b9f873f84750c8487d0e80
Moran Barzilay <34056415+MoranBarzilay@users.noreply.github.com>
2018-04-05 20:04:21 +0300

    Update LoadImages.js

--- JavaScript/LoadImages.js
+++ JavaScript/LoadImages.js
@@ -4,20 +4,18 @@
             contentType: 'html',
             url: path,
             success: function (data) {
-                var i = 0;
-                var imageCollection = $(data).find("a:contains(.jpeg)");
-                var length = imageCollection.length;
-                imageCollection.each(function () {
-                    var images = $(this).attr("href");
-                    $("#header ul").append('<li style="background-image: url(' + images + ');"></li>');
-                    if (i == length - 1) {
+                data = data.substring(1, data.length-1);
+                var url = this.url;
+                var res = data.split(";");
+                for(var i = 0; i < res.length; i++){
+                    $("#header ul").append('<li style="background-image: url(' + url + res[i] + ');"></li>');
+                    if (i == res.length - 1) {
                         $("#header ol").append('<li class="active" onclick="pointClick(' + i + ')"><i class="fa fa-circle-o"></i></li>');
                     }
                     else {
                         $("#header ol").append('<li onclick="pointClick(' + i + ')"><i class="fa fa-circle-o"></i></li>');
                     }
-                    i++;
-                });
+                }
             },
             error: function(XMLHttpRequest, textStatus, errorThrown) { 
                 alert("Status: " + textStatus);
